21ddc1cca1cc12cff7f5bc157cdff3a2360f95ce
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2013-01-28 19:26:49 +0200

    Moving food

---
+++ src/main/java/com/lagodiuk/agent/MovingFood.java
@@ -0,0 +1,26 @@
+package com.lagodiuk.agent;
+
+public class MovingFood extends Food {
+
+	private double angle;
+
+	private double speed;
+
+	public MovingFood(double x, double y, double angle, double speed) {
+		super(x, y);
+		this.speed = speed;
+		this.angle = angle;
+	}
+
+	@Override
+	public void interact(AgentsEnvironment env) {
+		this.move(env);
+	}
+
+	public void move(AgentsEnvironment env) {
+		double rx = -Math.sin(this.angle);
+		double ry = Math.cos(this.angle);
+		this.setX(this.getX() + (rx * this.speed));
+		this.setY(this.getY() + (ry * this.speed));
+	}
+}
